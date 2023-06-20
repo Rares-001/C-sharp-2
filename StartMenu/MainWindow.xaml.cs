f083e716c30923aa4f0fafc0fbf911570a2aaad6
Rares-001 <91318114+Rares-001@users.noreply.github.com>
2023-06-20 10:49:39 +0200

    Add files via upload
    The StartMenu includes a graphical user interface implemented using XAML .
    The main window of the application serves as the start menu.
    
    Additionally, includes various game-related content files such as arena, bullet, explosion, font, obstacle, player, pixel, and different o0f tiles and zombies. 
    
    Also contains build artifacts and intermediate files generated during the compilation process by MonoGame framework

---
+++ StartMenu/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StartMenu
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartGameButton_Click(object sender, RoutedEventArgs e)
+        {
+            string executablePath = System.IO.Path.Combine(
+                System.AppDomain.CurrentDomain.BaseDirectory, "Project002.exe");
+            System.Diagnostics.Process.Start(executablePath);
+        }
+    }
+}
